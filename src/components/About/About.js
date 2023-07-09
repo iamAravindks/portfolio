b9382e891b5ae8b4f74900b8d9df029109f77b02
iamAravindks <aravindkadaikattu@gmail.com>
2023-07-09 15:56:32 +0530

    make it better4

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,38 +1,47 @@
-import React from 'react';
-import './About.css'
+import React from "react";
+import "./About.css";
 
 function About() {
-	return (
-		<section>
-			<div className="left" id="about">
-				<h1 className="page-header">About Me</h1>
-			</div>
-			<div className="left">
-				<img
-					src={require('../../assets/images/avatar.jpeg')}
-					alt="about-me"
-					className="avatar"
-				/>
-			</div><br></br>
-			<div>
-				<p>
-				Hi there! I'm John Doe, a passionate and creative web developer with a strong foundation in front-end and back-end technologies. I have a deep love for coding and enjoy building responsive and user-friendly websites that leave a lasting impression on users.
-
-My journey as a developer began several years ago, and since then, I have continuously honed my skills and kept up with the latest industry trends. I specialize in using modern web technologies such as HTML5, CSS3, JavaScript, and React to craft intuitive and interactive web applications.
-
-As a problem solver by nature, I thrive on challenges and take pride in finding elegant solutions to complex problems. I'm a strong believer in the power of collaboration and enjoy working in teams to bring ideas to life.
-
-When I'm not coding, you can find me exploring new technologies, attending tech meetups, or contributing to open-source projects. I believe in the importance of giving back to the developer community and sharing knowledge.
-
-				</p>
-				<p>
-				If you have a project idea or just want to say hello, feel free to get in touch. I'm always excited to connect with fellow developers and collaborate on interesting projects!
-
-Let's create something amazing together!
-				</p>
-			</div>
-		</section>
-	);
+  return (
+    <section>
+      <div className="left" id="about">
+        <h1 className="page-header">About Me</h1>
+      </div>
+      <div className="left">
+        <img
+          src={require("../../assets/images/avatar.jpeg")}
+          alt="about-me"
+          className="avatar"
+        />
+      </div>
+      <br></br>
+      <div>
+        <p>
+          Hi there! I'm Moustapha Merhi, a passionate and creative web developer
+          with a strong foundation in front-end and back-end technologies. I
+          have a deep love for coding and enjoy building responsive and
+          user-friendly websites that leave a lasting impression on users. My
+          journey as a developer began several years ago, and since then, I have
+          continuously honed my skills and kept up with the latest industry
+          trends. I specialize in using modern web technologies such as HTML5,
+          CSS3, JavaScript, and React to craft intuitive and interactive web
+          applications. As a problem solver by nature, I thrive on challenges
+          and take pride in finding elegant solutions to complex problems. I'm a
+          strong believer in the power of collaboration and enjoy working in
+          teams to bring ideas to life. When I'm not coding, you can find me
+          exploring new technologies, attending tech meetups, or contributing to
+          open-source projects. I believe in the importance of giving back to
+          the developer community and sharing knowledge.
+        </p>
+        <p>
+          If you have a project idea or just want to say hello, feel free to get
+          in touch. I'm always excited to connect with fellow developers and
+          collaborate on interesting projects! Let's create something amazing
+          together!
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,118 +1,119 @@
-import React from 'react';
-import Project from '../Project/Project';
+import React from "react";
+import Project from "../Project/Project";
 
 function Portfolio() {
-	const projects = [
-		{
-			name: 'Project 1',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper malesuada urna, non tristique urna iaculis ac. Nulla viverra nulla eu tincidunt lacinia. Aliquam malesuada commodo sem id vestibulum. Donec maximus dui ut felis tristique dapibus. Nulla facilisi.',
-			image: 'work1.jpg',
-			technologies: [
-				'HTML/CSS',
-				'JavaScript',
-				'Node.js',
-				'Express & NoSQL',
-				'Heroku & AWS S3',
-				'React.js',
-				'MERN Stack',
-			],
-			github: 'https://github.com',
-			deployed: 'https://github.com',
-		},
-		{
-			name: 'Project 2',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper malesuada urna, non tristique urna iaculis ac. Nulla viverra nulla eu tincidunt lacinia. Aliquam malesuada commodo sem id vestibulum. Donec maximus dui ut felis tristique dapibus. Nulla facilisi.',
-			image: 'work2.jpg',
-			technologies: [
-				'HTML/CSS',
-				'JavaScript',
-				'Node.js',
-				'Handlebars.js',
-				'Express & Sequelize',
-				'Heroku & AWS S3',
-			],
-			github: 'https://github.com',
-			deployed: 'https://github.com',
-		},
-		{
-			name: 'Project 3',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper malesuada urna, non tristique urna iaculis ac. Nulla viverra nulla eu tincidunt lacinia. Aliquam malesuada commodo sem id vestibulum. Donec maximus dui ut felis tristique dapibus. Nulla facilisi.',
-			image: 'work3.jpeg',
-			technologies: ['HTML/CSS', 'JavaScript', 'jQuery'],
-			github: 'https://github.com',
-			deployed: 'https://github.com',
-		},
-		{
-			name: 'Project 4',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper malesuada urna, non tristique urna iaculis ac. Nulla viverra nulla eu tincidunt lacinia. Aliquam malesuada commodo sem id vestibulum. Donec maximus dui ut felis tristique dapibus. Nulla facilisi.',
-			image: 'work4.jpeg',
-			technologies: [
-				'JavaScript',
-				'MongoDB',
-				'IndexedDB & Service Workers',
-				'Node.js',
-				'Express',
-			],
-			github: 'https://github.com',
-			deployed: 'https://github.com',
-		},
-		{
-			name: 'Project 5',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper malesuada urna, non tristique urna iaculis ac. Nulla viverra nulla eu tincidunt lacinia. Aliquam malesuada commodo sem id vestibulum. Donec maximus dui ut felis tristique dapibus. Nulla facilisi.',
-			image: 'work2.jpg',
-			technologies: ['HTML/CSS', 'JavaScript'],
-			github: 'https://github.com',
-			deployed: 'https://github.com',
-		},
-		{
-			name: 'Project 6',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper malesuada urna, non tristique urna iaculis ac. Nulla viverra nulla eu tincidunt lacinia. Aliquam malesuada commodo sem id vestibulum. Donec maximus dui ut felis tristique dapibus. Nulla facilisi.',
-			image: 'work1.jpg',
-			technologies: ['HTML/CSS', 'JavaScript'],
-			github: 'https://github.com',
-			deployed: 'https://github.com',
-		}
-	];
+  const projects = [
+    {
+      name: "Portifolio",
+      description: "My portifolio built with Reactjs",
+      image: "work1.jpg",
+      technologies: [
+        "HTML/CSS",
+        "JavaScript",
+        "Node.js",
+        "Express & NoSQL",
+        "Heroku & AWS S3",
+        "React.js",
+        "MERN Stack",
+      ],
+      github: "https://github.com",
+      deployed: "https://github.com",
+    },
+    {
+      name: "Login system",
+      description:
+        "Login system that is built using Nodejs ,expressjs and mysql .It enables the features like user registration , login , logout and profile view and update , ",
+      image: "work2.jpg",
+      technologies: [
+        "HTML/CSS",
+        "JavaScript",
+        "Node.js",
+        "Handlebars.js",
+        "Express & Sequelize",
+        "Heroku & AWS S3",
+      ],
+      github: "https://github.com",
+      deployed: "https://node-login-3b72754928dd.herokuapp.com/",
+    },
+    {
+      name: "PWA",
+      description:
+        "This application takes an existing text editor app and adds functionality for it to work as a PWA and function offline. This application is deployed through Heroku.",
+      image: "work3.jpeg",
+      technologies: ["HTML/CSS", "JavaScript", "Nodejs", "Expressjs"],
+      github: "https://github.com",
+      deployed: "https://github.com",
+    },
+    {
+      name: "Password generator",
+      description:
+        "A web app that randomly generate a password that meets certain criteria.create a strong password that provides greater security",
+      image: "work4.jpeg",
+      technologies: ["HTML/CSS", "JavaScript"],
+      github: "https://github.com",
+      deployed: "https://github.com",
+    },
+    {
+      name: "Coding Quiz",
+      description:
+        "AS A coding boot camp student I WANT to take a timed quiz on JavaScript fundamentals that stores high scores SO THAT I can gauge my progress compared to my peers",
+      image: "work2.jpg",
+      technologies: ["HTML/CSS", "JavaScript"],
+      github: "https://github.com",
+      deployed: "https://github.com",
+    },
+    {
+      name: `Owner avatar logo-maker`,
+      description:
+        'The application enables users to enter inputs into a inquirer prompt in order to generate a logo "logo.svg" file which contains the users desired results as an SVG image.',
+      image: "work1.jpg",
+      technologies: [
+        "Nodejs",
 
-	return (
-		<section>
-			<div className="left">
-				<h1 className="page-header">My Portfolio</h1>
-			</div>
-			<div>
-				<ul className="flex-row mobile-row">
-					<li className="padding">
-						<Project projects={projects[0]}></Project>
-					</li>
-					<li className="padding">
-						<Project projects={projects[1]}></Project>
-					</li>
-				</ul>
-				<ul className="flex-row mobile-row">
-					<li className="padding">
-						<Project projects={projects[2]}></Project>
-					</li>
-					<li className="padding">
-						<Project projects={projects[3]}></Project>
-					</li>
-				</ul>
-				<ul className="flex-row mobile-row">
-					<li className="padding">
-						<Project projects={projects[4]}></Project>
-					</li>
-					<li className="padding">
-						<Project projects={projects[5]}></Project>
-					</li>
-				</ul>
-			</div>
-		</section>
-	);
+        "    Saiyan Pride",
+        "    Dynamic JavaScript",
+        " Node.js ",
+        " Jest ",
+        "Inquirer.js",
+      ],
+      github: "https://github.com",
+      deployed: "https://github.com",
+    },
+  ];
+
+  return (
+    <section>
+      <div className="left">
+        <h1 className="page-header">My Portfolio</h1>
+      </div>
+      <div>
+        <ul className="flex-row mobile-row">
+          <li className="padding">
+            <Project projects={projects[0]}></Project>
+          </li>
+          <li className="padding">
+            <Project projects={projects[1]}></Project>
+          </li>
+        </ul>
+        <ul className="flex-row mobile-row">
+          <li className="padding">
+            <Project projects={projects[2]}></Project>
+          </li>
+          <li className="padding">
+            <Project projects={projects[3]}></Project>
+          </li>
+        </ul>
+        <ul className="flex-row mobile-row">
+          <li className="padding">
+            <Project projects={projects[4]}></Project>
+          </li>
+          <li className="padding">
+            <Project projects={projects[5]}></Project>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
